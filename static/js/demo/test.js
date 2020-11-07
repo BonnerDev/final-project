a101ca5b33bad8bbdb9253488847e5f0b7168e55
BAS <63766016+BonnerDev@users.noreply.github.com>
2020-11-07 14:32:17 +0800

    push2

--- static/js/demo/test.js
+++ static/js/demo/test.js
@@ -1,5 +1,11 @@
-function bartest(){
-    d3.json("/data").then((test) => {
-        console.log(test)
+console.log("hello")
+
+var data;
+
+d3.json("/data",(test)=>{
+    console.log(test)
+    Object.values(test).forEach
+    ((element) => {
+        console.log(element.placement_type)
     });
-};
+});
